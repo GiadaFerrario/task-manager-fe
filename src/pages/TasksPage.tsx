3ec650c05582c92440ab3db26118df2cde28786f
giadaferrario <giada.ferrario@student.supsi.ch>
2025-11-03 22:06:17 +0100

    add: page folder

---
+++ src/pages/TasksPage.tsx
@@ -0,0 +1,15 @@
+import {
+    Typography,
+} from "@mui/material";
+
+export default function TasksPage() {
+
+    return (
+        <>
+            <Typography variant="h5" gutterBottom>
+                Implement tasks page
+            </Typography>
+        </>
+
+    );
+}
